b0a87b92737449ea9efdb4cee37a8b1e2c315eef
elfarsif <f.n.elfarsi@gmail.com>
2026-02-22 11:00:30 -0500

    added zsh-autosuggestions, syntax, z

--- install.sh
+++ install.sh
@@ -28,4 +28,21 @@ else
     echo "Oh My Zsh already installed"
 fi
 
+# Zsh plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+    echo "zsh-autosuggestions already installed"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+else
+    echo "zsh-syntax-highlighting already installed"
+fi
+
 echo "Done!"
